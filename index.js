1e8d64ceec5a97d7ac1545642c3ec2ab0949c762
bramapeters <bramapeters@hotmail.com>
2021-01-11 14:15:06 +0100

    Documentation
    Documentation

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
-// Conversion factors households
-var house_after_elec = 20.8
-var house_after_fuel = 62.3
-var house_after_gas = 48.6
-var house_before_elec = 25.2
-var house_before_fuel = 78.1
-var house_before_gas = 60.4
-var apart_after_elec = 18.0
-var apart_after_fuel = 49.7
-var apart_after_gas = 40.8
-var apart_before_elec = 23.2
-var apart_before_fuel = 55.9
-var apart_before_gas = 54.6
+// Conversion factors households (per 1m^2) ref: https://ecotree.green/nl/calculate-home-co2
+var house_after_elec = 20.8         // house + recent (after 1975) + electricity heating = 20.8 kg CO2 (per 1m^2)
+var house_after_fuel = 62.3         // house + recent (after 1975) + fuel oil heating = 62.3 kg CO2 (per 1m^2)
+var house_after_gas = 48.6          // house + recent + gas heating = 48.6 kg CO2 (per 1m^2)
+var house_before_elec = 25.2        // house + old (pre 1975) + electricity heating = 25.2 kg CO2 (per 1m^2)
+var house_before_fuel = 78.1        // house + old + fuel oil = 78.1 kg CO2 (per 1m^2)
+var house_before_gas = 60.4         // house + old + gas = 60.4 kg CO2 (per 1m^2)
+var apart_after_elec = 18.0         // apartment + recent + electricity = 18.0 kg CO2 (per 1m^2)
+var apart_after_fuel = 49.7         // apartment + recent + fuel oil = 49.7 kg CO2 (per 1m^2)
+var apart_after_gas = 40.8          // apartment + recent + gas = 40.8 kg CO2 (per 1m^2)
+var apart_before_elec = 23.2        // apartment + old + electricity = 23.2 kg CO2 (per 1m^2)
+var apart_before_fuel = 55.9        // apartment + old + fuel oil = 55.9 kg CO2 (per 1m^2)
+var apart_before_gas = 54.6         // apartment + old + gas = 54.6 kg CO2 (per 1m^2)
 
-// Average electricity use per household
-var elec_avg_1 = 1850
-var elec_avg_2 = 2860
-var elec_avg_3 = 3400
-var elec_avg_4 = 3930
-var elec_avg_5 = 4180
-var elec_emission = 0.4570
+// Average annual electricity use per household (in kWh) ref: https://www.carbonfootprint.com/docs/2019_06_emissions_factors_sources_for_2019_electricity.pdf
+var elec_avg_1 = 1850 // kWh annually for 1-person household
+var elec_avg_2 = 2860 // kWh annually for 2-person household
+var elec_avg_3 = 3400 // kWh annually for 3-person household
+var elec_avg_4 = 3930 // kWh annually for 4-person household
+var elec_avg_5 = 4180 // kWh annually for 5-person household
+var elec_emission = 0.4570 // electricity mission factor for Netherlands in kg CO2e/kWh
 
-// Food conversion factor
-var food_weight_meat = 0.25
-var food_emission_meat = 17
-var food_weight_dairy = 0.7
-var food_emission_dairy = 6
+// Food conversion factor ref: https://www.rivm.nl/bibliotheek/rapporten/2016-0195.pdf page 35
+var food_weight_meat = 0.25 // (kg) average daily intake for meat
+var food_emission_meat = 17 // (kg CO2 eq) emission factor per kg of meat
+var food_weight_dairy = 0.7 // (kg) average daily intake for dairy
+var food_emission_dairy = 6 // (kg CO2 eq) emission factor per kg of dairy
 
-// Car conversion factor
-var carbon_emission_diesel = 3.32
-var carbon_emission_petrol = 2.74
-var carbon_emission_electric = 9.5
+// Car conversion factor ref: https://www.eea.europa.eu/data-and-maps/indicators/average-co2-emissions-from-motor-vehicles/assessment-2
+var carbon_emission_diesel = 3.32 // (kg CO2) average emission per liter diesel
+var carbon_emission_petrol = 2.74 // (kg CO2) average emission per liter gasoline
+var carbon_emission_electric = 9.5 // (kg CO2) average emission per 100 km
 
-// Public transport conversion factor
-var weeks_per_year = 48
-var publictransport_emission = 0.0266
+// Public transport conversion factor ref: co2emissiefactoren.nl/wp-content/uploads/2017/12/Emissiecijfers-openbaar-vervoer-dec-2017.pdf
+var weeks_per_year = 48 // weeks per year
+var publictransport_emission = 0.0266 // (kg CO2) average per passenger km
 
-// Plane conversion factor
-var plane_avg_speed = 907
-var plane_emission_km = 125
+// Plane conversion factor ref: https://www.statista.com/statistics/1113177/co2-emissions-by-airline-europe/
+var plane_avg_speed = 907 // average cruising speed airplane
+var plane_emission_km = 125 // (g CO2) average per passenger km
 
-// Clothes conversion factor
-var clothes_weight = 0.4
-var clothes_emission = 14.42
-var clothes_quarter = 4
+// Clothes conversion factor ref: https://www.researchgate.net/publication/276193965_Carbon_Footprint_of_Textile_and_Clothing_Products
+var clothes_weight = 0.4 // (kg) average weight of piece of clothing
+var clothes_emission = 14.42 // (kg CO2) average emission per kg of fabric (average of multiple fabric categories)
+var clothes_quarter = 4 // quartiles per year
 
 // Final emission scores
-var emission_housing
-var emission_electricity
-var emission_meat_dairy
-var emission_car
-var emission_public_transport
-var emission_plane
-var emission_clothes
-var emission_total
+var emission_housing            // total CO2 emission housing category
+var emission_electricity        // total CO2 emission electricity category
+var emission_meat_dairy         // total CO2 emission food consumption category
+var emission_car                // total CO2 emission car travel category
+var emission_public_transport   // total CO2 emission public transport category
+var emission_plane              // total CO2 emission plane travel category
+var emission_clothes            // total CO2 emission clothing category
+var emission_total              // total CO2 emission all categories
 
-// Definining visualization verification code
+// Definining default visualization verification code
 var verification_code = "..."
 
-// Survey initialization
+// SurveyJS initialization
 Survey
     .StylesManager
     .applyTheme("modern");
@@ -75,14 +75,13 @@ Survey
         isLocalizable: true
     });
 
-// Survey object
+// Survey JSON-object
 var json = {
-    //"title": "Minimum data reporting form – for suspected and probable cases of COVID-19",
     "pages": [
         {
+            // Landing page
             "name": "page1",
             "navigationTitle": "Introduction",
-            //"navigationDescription": "...",
             "elements": [
                 {
                     type: "html",
@@ -92,6 +91,7 @@ var json = {
                 }
             ],
         }, {
+            // Household emission category page
             "name": "page2",
             "navigationTitle": "Heating",
             "elements": [
@@ -143,6 +143,7 @@ var json = {
                 }
             ]
         }, {
+            // Electricity consumption emission category page
             "name": "page3",
             "navigationTitle": "Electricity",
             "elements": [
@@ -204,6 +205,7 @@ var json = {
                 }
             ]
         }, {
+            // Food consumption emission category page
             "name": "page4",
             "navigationTitle": "Food",
             "elements": [
@@ -242,6 +244,7 @@ var json = {
                 }
             ]
         }, {
+            // Travel emission category page
             "name": "page5",
             "navigationTitle": "Travel",
             "elements": [
@@ -315,6 +318,7 @@ var json = {
                 }
             ]
         }, {
+            // Clothing emission category page
             "name": "page6",
             "navigationTitle": "Clothing",
             "elements": [
@@ -355,14 +359,14 @@ var json = {
     ]
 };
 
-// Initialize survey object
+// Initialize survey JSON-object
 window.survey = new Survey.Model(json);
 
 // Do when survey completed
 survey
     .onComplete
     .add(function (result) {
-        // Get survey input values
+        // Get survey input values and save to new variables
         var household_number = result.getValue("household_number");
         var household_type = result.getValue("household_type");
         var household_heat = result.getValue("household_heat");
@@ -385,16 +389,16 @@ survey
         var plane_distance = result.getValue("plane_distance");
         var clothes_amount = result.getValue("clothes_amount");
         var clothes_used = result.getValue("clothes_used");
+        // Call calculate score function and pass survey input values as argument
         calcScore(household_number, household_type, household_heat, household_size, electricity_type,
             electricity_amount_1, electricity_amount_2, electricity_amount_3, electricity_amount_4, electricity_amount_5,
             food_meat, food_dairy, food_vegetables, car_travel, car_type, car_share, car_distance, train_distance, publictransport_distance,
             plane_distance, clothes_amount, clothes_used);
+        // Generate string representation of SurveyJS JSON-object
         document
             .querySelector('#surveyResult')
             .textContent = "Result JSON:\n" + JSON.stringify(result.data, null, 3);
-        //window.location.href = "result.html";
     });
-
 $("#surveyElement").Survey({model: survey});
 
 // Calculate carbon footprint score
@@ -566,15 +570,13 @@ function calcScore(household_number, household_type, household_heat, household_s
     // Calculate total emission footprint
     emission_total = (emission_housing + emission_electricity + emission_meat_dairy + emission_car + emission_public_transport + emission_plane + emission_clothes).toFixed(2);
 
-    // Show visualization
+    // Call show visualization function
     showVisualization(emission_total)
-    //var wnd = window.open("about:blank", "", "_blank");
-    //wnd.document.write("Your household emission is "+ emission_housing + ". Your " + electricity_type + " emission is " + emission_electricity + " for a household of " + household_number + ". Your clothing emission is " + emission_clothes + ". Your meat and dairy emission is " + emission_meat_dairy + ". Your car travel emission is " + emission_car + ". Your public transport emission is " + emission_public_transport + ". Your plane travel emission is " + emission_plane + ". Your total emission is " + emission_total + ".");
 }
 
 // Show visualization function
 function showVisualization(emission_total) {
-    // Hide survey element
+    // Hide SurveyJS element
     document.getElementById("surveyNavigation").style.display = "none";
     document.getElementById("surveyElement").style.display = "none";
     document.getElementById("surveyResult").style.display = "none";
@@ -582,7 +584,7 @@ function showVisualization(emission_total) {
     // Show load icon
     document.getElementById("load_icon").style.display = "inline-block";
 
-    // Show fade out screens
+    // Temporarily dim screens
     document.getElementById("overlay_1").style.display = "inline-block";
     document.getElementById("overlay_2").style.display = "inline-block";
     document.getElementById("overlay_3").style.display = "inline-block";
@@ -591,7 +593,7 @@ function showVisualization(emission_total) {
     google.charts.load('current', {'packages': ['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
-    // Draw the chart and set the chart values
+    // Draw piechart and set chart values
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
             ['Attribute', 'Emission'],
@@ -603,7 +605,6 @@ function showVisualization(emission_total) {
             ['Public Transport', Math.round(emission_public_transport)],
             ['Clothing', Math.round(emission_clothes)]
         ]);
-
         var options = {
             'width': 450, 'height': 450,
             'backgroundColor': 'transparent',
@@ -630,13 +631,14 @@ function showVisualization(emission_total) {
             },
         };
 
-        // Display the chart inside the <div> element with id="piechart"
+        // Display the chart inside <div> element with id="piechart"
         var chart = new google.visualization.PieChart(document.getElementById('piechart'));
         chart.draw(data, options);
     }
 
-    // Randomize for one out of three visualizations
+    // Random number generator to randomize for one out of three possible visualizations
     var random_number = Math.floor(Math.random() * 3) + 1
+    // Display globe, numerical or social comparison visualization based on random number
     if (random_number == 1) {
         // Globe visualization
         if (emission_total < 3900){
@@ -660,7 +662,9 @@ function showVisualization(emission_total) {
         } else if (emission_total >= 27100){
             verification_code = "GV_10";
         }
+        // Show appropriate verification code
         document.getElementById("verification_code").innerText = "VERIFICATION CODE: " + verification_code
+        // Load and display correct globe visualization image
         document.getElementById("result_image").src = "globes_images/" + verification_code + ".png";
         document.getElementById("image_visualization").style.display = "inline";
     } else if (random_number == 2) {
@@ -686,7 +690,9 @@ function showVisualization(emission_total) {
         } else if (emission_total >= 27100){
             verification_code = "NV_10";
         }
+        // Show appropriate verification code
         document.getElementById("verification_code").innerText = "VERIFICATION CODE: " + verification_code
+        // Load and display correct numerical visualization value
         document.getElementById("numerical_text").innerHTML = "<b>" + emission_total + " kg CO<sub>2</sub></b>"
         document.getElementById("numerical_visualization").style.display = "inline";
     } else if (random_number == 3) {
@@ -712,7 +718,9 @@ function showVisualization(emission_total) {
         } else if (emission_total >= 27100){
             verification_code = "SCV_10";
         }
+        // Show appropriate verification code
         document.getElementById("verification_code").innerText = "VERIFICATION CODE: " + verification_code
+        // Load and display correct social comparison image
         document.getElementById("result_image").src = "socialcomparison_images/" + verification_code + ".png";
         document.getElementById("image_visualization").style.display = "inline";
     }
@@ -720,6 +728,7 @@ function showVisualization(emission_total) {
     // Show visualization
     document.getElementById("visualization").style.display = "inline";
 
+    // Wait 2 seconds before screen dim reverted and load icon removed
     setTimeout(function () {
         // Hide fade out screens
         document.getElementById("overlay_1").style.display = "none";
@@ -741,6 +750,7 @@ function copyToClipboard() {
     window.alert("Your verification code has been copied to your clipboard! You can now simply paste it elsewhere.");
 }
 
+// SurveyJS
 var navTopEl = document.querySelector("#surveyNavigationTop");
 navTopEl.className = "navigationContainer";
 var leftImg = document.createElement("img");
